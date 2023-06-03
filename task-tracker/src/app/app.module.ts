0d49e85e817c962f54478f138ff3dc2774e568be
BenVelte <Ben.V@t-online.de>
2023-06-03 19:04:06 +0200

    Add Form component design

--- task-tracker/src/app/app.module.ts
+++ task-tracker/src/app/app.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzListModule } from 'ng-zorro-antd/list';
 
 import { AppComponent } from './app.component';
 import { TaskListComponent } from './task-list/task-list.component';
 import { TaskFormComponent } from './task-form/task-form.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { de_DE } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import de from '@angular/common/locales/de';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+registerLocaleData(de);
 
 @NgModule({
   declarations: [
@@ -14,9 +25,17 @@ import { TaskFormComponent } from './task-form/task-form.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    NzFormModule,
+    NzButtonModule,
+    NzInputModule,
+    NzListModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    { provide: NZ_I18N, useValue: de_DE }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
